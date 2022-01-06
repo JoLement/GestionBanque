0cd999890df9b11986f674cf132b7f8ddfaa3487
JoLement <86296724+JoLement@users.noreply.github.com>
2022-01-06 22:37:00 +0100

    Ajoutez des fichiers projet.

---
+++ UnitTestBasicMaths/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBasicMaths")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBasicMaths")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88960e09-5e29-415b-9eef-92d179035a95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestBasicMaths/UnitTestMaths.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Banque;
+
+namespace UnitTestBasicMaths
+{
+    [TestClass]
+    public class UnitTestMaths
+    { 
+    [TestMethod]
+    public void BasicMaths_DivisionParZero()
+    {
+        // Arrange
+        double num1 = 10;
+        double num2 = 0;
+
+        BasicMaths math = new BasicMaths();
+        // Act
+        try
+        {
+            math.divide(num1, num2);
+        }
+        catch (System.DivideByZeroException)
+        {
+            // Assert
+            Assert.ThrowsException<System.DivideByZeroException>(() => math.divide(num1, num2));
+            return;
+        }
+
+
+    }
+    [TestMethod]
+    public void BasicMaths_PLusEqual()
+    {
+        // Arrange
+        double num1 = 5;
+        double num2 = 2;
+        double expected = 7;
+        BasicMaths math = new BasicMaths();
+        // Act
+        double actual = math.Add(num1, num2);
+        // Assert
+
+        Assert.AreEqual(expected, actual, 0.001, "Problème dans la méthode Add");
+    }
+
+    [TestMethod]
+    public void BasicMaths_MinusEqual()
+    {
+        // Arrange
+        double num1 = 5;
+        double num2 = 2;
+        double expected = 3;
+        BasicMaths math = new BasicMaths();
+        // Act
+        double actual = math.Substract(num1, num2);
+        // Assert
+
+        Assert.AreEqual(expected, actual, 0.001, "Problème dans la méthode Add");
+    }
+    [TestMethod]
+    public void BasicMaths_divideEqual()
+    {
+        // Arrange
+        double num1 = 6;
+        double num2 = 2;
+        double expected = 3;
+        BasicMaths math = new BasicMaths();
+        // Act
+        double actual = math.divide(num1, num2);
+        // Assert
+
+        Assert.AreEqual(expected, actual, 0.001, "Problème dans la méthode Add");
+    }
+    [TestMethod]
+    public void BasicMaths_MultiplyEqual()
+    {
+        // Arrange
+        double num1 = 5;
+        double num2 = 2;
+        double expected = 10;
+        BasicMaths math = new BasicMaths();
+        // Act
+        double actual = math.Multiply(num1, num2);
+        // Assert
+
+        Assert.AreEqual(expected, actual, 0.001, "Problème dans la méthode Add");
+    }
+}
+}
